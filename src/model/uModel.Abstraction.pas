34fc5e5da97cce998ce089bb212f5655481fec55
Marcio Silva <m.s.copr@gmail.com>
2025-01-30 09:20:45 -0300

    :sparkles: WKComercial
    Criação da camada de abstração de acesso a dados da aplicação
    
    - Definição da interface `IDataManager` para gerenciamento de conexão e transações.
    
    - Implementação de `IMemory` para manipulação de `TFDMemTable`.
    
    - Introdução de `IStatement` para abstração de consultas SQL com `TFDQuery`.
    
    - Criação de interfaces genéricas para persistência:
    
      - `IRootPersistence` define métodos básicos como `FindAll()`.
    
      - `IPersistence` expande a persistência com operações CRUD.
    
      - `IRepository`, `IService`, `IController` e `IRootController` especializam a lógica de acesso e manipulação dos dados.
    
    - Adição de `IDataConverter` para conversão de listas de objetos para datasets.
    
    - Introdução do enum `TState` para controle do estado da entidade.
    
    Essas implementações estabelecem uma base para um modelo de persistência estruturado e desacoplado.

---
+++ src/model/uModel.Abstraction.pas
@@ -0,0 +1,100 @@
+unit uModel.Abstraction;
+
+interface
+
+uses
+  System.Classes, System.SysUtils, System.Generics.Collections, Data.DB, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
+  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+  FireDAC.Stan.Async, FireDAC.DApt;
+
+type
+  TState = (dsBrowse, dsInsert, dsEdit, dsOpening);
+
+  IDataManager = interface
+  ['{2D3401A0-6832-44CB-8FAB-22E9C37EC9D4}']
+    function GetConnection(): TCustomConnection;
+    function GetStartTransaction(): IDataManager;
+    function GetCommit(): IDataManager;
+    function GetRollback(): IDataManager;
+    function GetEntity(EntitName: String): IDataManager;
+    function GetFieldNames(): TStrings;
+
+    property Connection: TCustomConnection read GetConnection;
+
+    property StartTransaction: IDataManager read GetStartTransaction;
+    property Commit: IDataManager read GetCommit;
+    property Rollback: IDataManager read GetRollback;
+  end;
+
+  IMemory = interface
+  ['{B7F3A945-ACDE-4EC2-9C47-BAD69B86D9E4}']
+    function GetData(): TFDMemTable;
+
+    procedure SetRecNo(const Value: Integer);
+    function GetRecNo: Integer;
+
+    procedure Open();
+
+    property Data: TFDMemTable read GetData;
+    property RecNo: Integer read GetRecNo write SetRecNo;
+  end;
+
+  IStatement = interface
+  ['{21619B6D-AEB6-4B4F-8343-625CE14F6F57}']
+    function GetQuery(): TFDQuery;
+    function SQL(const Value: String): IStatement;
+    function Open(): IStatement;
+
+    property Query: TFDQuery read GetQuery;
+  end;
+
+  IRootPersistence<T: class> = interface
+  ['{D9047AE9-B05C-4B37-8AEE-9A6B9A5772EB}']
+    function Fields(): TStrings;
+    function FindAll(CommadSQL: String): TObjectList<T>;
+  end;
+
+  IPersistence<T: class> = interface(IRootPersistence<T>)
+  ['{98D74E55-3050-4E35-95E0-74A8B349D6FB}']
+    function Save(Entity: T): Boolean;
+    function Update(Entity: T): Boolean;
+    function DeleteById(Entity: T): Boolean;
+    function FindById(Id: Integer): T;
+    function FindAll(): TObjectList<T>; overload;
+  end;
+
+  IRootController<T: class> = interface(IRootPersistence<T>)
+  ['{DC75061F-D9C5-4BAA-A226-2A3FE1D36DE1}']
+  end;
+
+  IController<T: class> = interface(IPersistence<T>)
+  ['{0C7B72B1-76BA-4463-BA7F-FAC7A90470EC}']
+  end;
+
+  IService<T: class> = interface(IPersistence<T>)
+  ['{5E47E4E7-D251-4AD9-BA5E-00B5DBAAD58C}']
+    function IsValid(Entity: T; out MessageContext: String): Boolean;
+  end;
+
+  IRepository<T: class> = interface(IPersistence<T>)
+  ['{C192836A-9BF1-43CC-B8FE-1496A6A71705}']
+    procedure SetStatement(Statement: IStatement; Entity: T);
+    procedure SetProperty(Statement: IStatement; Entity: T);
+    procedure PopulateListEntitie(var List: TObjectList<T>; const Statement: IStatement);
+  end;
+
+  IDataConverter<T: class> = interface
+  ['{D27C0689-A569-4DC3-B1B1-3FA4233B0E5C}']
+    procedure Populate(Source: TObjectList<T>; Target: TDataSet);
+  end;
+
+var
+  State: TState;
+
+  StateItems: TState;
+
+implementation
+
+end.
+
+
