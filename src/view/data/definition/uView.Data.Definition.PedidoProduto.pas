a04b60b818e85b99959b9bbb2e4e97270e473867
Marcio Silva <m.s.copr@gmail.com>
2025-01-31 16:11:06 -0300

    :sparkles: Criado a classes "TDataDefinitionPedidoProduto"
    - Implementado a classe "TDataDefinitionPedidoProduto", a mesma é responsável pela da estrutura dos Fields de um "TModelFireDACMemory" para a tabela de pedidos_produtos.
    
    - A classe contém um método "JSON" que retorna uma string JSON com a estrutura dos campos utilizados no "TModelFireDACMemory", incluindo seus tipos e formatos de exibição.

---
+++ src/view/data/definition/uView.Data.Definition.PedidoProduto.pas
@@ -0,0 +1,34 @@
+unit uView.Data.Definition.PedidoProduto;
+
+interface
+
+type
+  TDataDefinitionPedidoProduto = class
+  public
+    class function JSON(): String;
+  end;
+
+implementation
+
+{ TDataDefinitionPedidoProduto }
+
+uses
+  Data.DB;
+
+class function TDataDefinitionPedidoProduto.JSON(): String;
+begin
+  Result := '{' +
+             '"fields": [' +
+             '  { "name": "data_emissao", "type": "ftDate" },' +
+             '  { "name": "numero_pedido", "type": "ftInteger" },' +
+             '  { "name": "autoincrem", "type": "ftInteger" },' +
+             '  { "name": "codigo_produto", "type": "ftInteger" },' +
+             '  { "name": "nome_produto", "type": "ftWideString", "length": 255 },' +
+             '  { "name": "quantidade", "type": "ftCurrency", "displayFormat": "#,##0.00" },' +
+             '  { "name": "valor_unitario", "type": "ftCurrency", "displayFormat": "#,##0.00" },' +
+             '  { "name": "valor_total", "type": "ftCurrency", "displayFormat": "#,##0.00" }' +
+             ']' +
+             '}';
+end;
+
+end.
