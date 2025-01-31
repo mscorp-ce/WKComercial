8b395cc5e6777ecf051e48760c75a1a52116d89e
Marcio Silva <m.s.copr@gmail.com>
2025-01-31 18:29:52 -0300

    :sparkles: Criada a unit uModel.ConstsStatement.PedidoDadosGerais, contendo constantes SQL para manipulação de dados no contexto de "pedidos_dados_gerais".
    - Definidas as queries:
       - QUERY_PEDIDO_DADOS_GERAIS: consulta básica de dados gerais do pedido.
    
       - QUERY_ORDER_BY_PEDIDO_DADOS_GERAIS: ordenação da consulta de pedidos_dados_gerais pelos campos: "data_emissao" e "numero_pedido".
    
       - QUERY_PEDIDO_DADOS_GERAIS_ORDER_BY: junção das constantes "QUERY_PEDIDO_DADOS_GERAIS" com "QUERY_ORDER_BY_PEDIDO_DADOS_GERAIS".
    
       - QUERY_INSERT_PEDIDO_DADOS_GERAIS: query para inserir dados gerais de pedido.
    
       - QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO: cláusula WHERE com filtro por número do pedido.
    
       - QUERY_PEDIDO_DADOS_GERAIS_BY_NUMERO_PEDIDO: junção das constantes "QUERY_PEDIDO_DADOS_GERAIS" com QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO".
    
       - QUERY_DADOS_GERAIS_CLAUSE_WHERE_BY_NUMERO_PEDIDO: junção das constantes "QUERY_PEDIDO_DADOS_GERAIS" com "QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO".
    
       - QUERY_UPDATE_DADOS_GERAIS_BY_NUMERO_PEDIDO: query de atualização de dados gerais de pedido.
    
       - QUERY_DELETE_DADOS_GERAIS_BY_NUMERO_PEDIDO_NUMERO_PEDIDO: query para excluir dados gerais de pedido com base no número do pedido.

---
+++ src/model/repository/uModel.ConstsStatement.PedidoDadosGerais.pas
@@ -0,0 +1,47 @@
+unit uModel.ConstsStatement.PedidoDadosGerais;
+
+interface
+
+uses
+  uModel.ConstsStatement.Repository.Commun;
+
+const
+  QUERY_PEDIDO_DADOS_GERAIS =
+    '   SELECT PDG.data_emissao, ' + sLineBreak +
+    '          PDG.numero_pedido, ' + sLineBreak +
+    '          PDG.codigo_cliente, ' + sLineBreak +
+    '          PDG.valor_total ' + sLineBreak +
+    '     FROM pedidos_dados_gerais PDG ';
+
+  QUERY_ORDER_BY_PEDIDO_DADOS_GERAIS =
+    ' ORDER BY PDG.data_emissao,  ' + sLineBreak +
+    '          PEP.numero_pedido ';
+
+  QUERY_PEDIDO_DADOS_GERAIS_ORDER_BY = QUERY_PEDIDO_DADOS_GERAIS + sLineBreak + QUERY_ORDER_BY_PEDIDO_DADOS_GERAIS;
+
+  QUERY_INSERT_PEDIDO_DADOS_GERAIS =
+    'INSERT INTO pedidos_dados_gerais (data_emissao, codigo_cliente, valor_total) ' + sLineBreak +
+    'VALUES (:data_emissao, :codigo_cliente, :valor_total) '+ sLineBreak +
+    'SELECT LAST_INSERT_ID() AS numero_pedido; ';
+
+  QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO = ' WHERE numero_pedido = :numero_pedido ';
+
+  QUERY_PEDIDO_DADOS_GERAIS_BY_NUMERO_PEDIDO = QUERY_PEDIDO_DADOS_GERAIS + sLineBreak + QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO;
+
+  QUERY_DADOS_GERAIS_CLAUSE_WHERE_BY_NUMERO_PEDIDO = QUERY_PEDIDO_DADOS_GERAIS + sLineBreak + QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO;
+
+  QUERY_UPDATE_DADOS_GERAIS_BY_NUMERO_PEDIDO =
+    'UPDATE pedidos_produtos ' + sLineBreak +
+    '   SET quantidade = :quantidade, ' + sLineBreak +
+    '       valor_unitario = :valor_unitario, ' + sLineBreak +
+    '       valor_total = valor_total ' + sLineBreak +
+    QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO;
+
+  QUERY_DELETE_DADOS_GERAIS_BY_NUMERO_PEDIDO_NUMERO_PEDIDO =
+    'DELETE ' + sLineBreak +
+    '  FROM pedidos_produtos ' + sLineBreak +
+    QUERY_CLAUSE_WHERE_BY_NUMERO_PEDIDO;
+
+implementation
+
+end.
