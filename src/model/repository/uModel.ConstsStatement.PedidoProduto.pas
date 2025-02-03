3b856571f20f5071388414068a51bc7e68028593
Marcio Silva <m.s.copr@gmail.com>
2025-02-03 08:11:36 -0300

    Criado a unit: "uModel.ConstsStatement.PedidoProduto" com as constantes SQL para manipulação de pedidos_produtos.
    Arquivo:
    
    - uModel.ConstsStatement.PedidoProduto.pas
    
    - Criadas querys SQL (SELECT, INSERT, UPDATE e DELETE) para gerenciar produtos vinculados a pedidos.
    
    - Definidas cláusulas de ORDER BY e WHERE para facilitar a reutilização das queries.
    
    - Implementado LAST_INSERT_ID() para obter o último registro inserido.
    
    - Estruturadas consultas relacionando pedidos_produtos, produtos e pedidos_dados_gerais.
    
    - Centralização de constantes SQL para facilitar manutenção e reutilização no repositório.

---
+++ src/model/repository/uModel.ConstsStatement.PedidoProduto.pas
@@ -0,0 +1,68 @@
+unit uModel.ConstsStatement.PedidoProduto;
+
+interface
+
+uses
+  uModel.ConstsStatement.Repository.Commun;
+
+const
+  QUERY_PEDIDO_DADOS_GERAIS =
+    '   SELECT PDG.data_emissao, ' + sLineBreak +
+    '          PEP.numero_pedido, ' + sLineBreak +
+    '          PEP.autoincrem, ' + sLineBreak +
+    '          PEP.codigo_produto, ' + sLineBreak +
+    '          PRO.descricao AS nome_produto, ' + sLineBreak +
+    '          PEP.quantidade, ' + sLineBreak +
+    '          PEP.valor_unitario, ' + sLineBreak +
+    '          PEP.valor_total ' + sLineBreak +
+    '     FROM pedidos_produtos PEP ' + sLineBreak +
+    '     JOIN produtos PRO ON PEP.codigo_produto = PRO.codigo ' + sLineBreak +
+    '     JOIN pedidos_dados_gerais PDG ON PEP.numero_pedido = PDG.numero_pedido ';
+
+  QUERY_PEDIDO_PRODUTOS =
+    '   SELECT PEP.autoincrem, ' + sLineBreak +
+    '          PEP.numero_pedido, ' + sLineBreak +
+    '          PEP.codigo_produto, ' + sLineBreak +
+    '          PEP.quantidade, ' + sLineBreak +
+    '          PEP.valor_unitario, ' + sLineBreak +
+    '          PEP.valor_total ' + sLineBreak +
+    '     FROM pedidos_produtos PEP ';
+
+  QUERY_ORDER_BY_PEDIDO_PRODUTO =
+    ' ORDER BY PEP.numero_pedido,  ' + sLineBreak +
+    '          PEP.autoincrem ';
+
+  QUERY_PEDIDO_PRODUTO_ORDER_BY = QUERY_PEDIDO_DADOS_GERAIS + sLineBreak + QUERY_ORDER_BY_PEDIDO_PRODUTO;
+
+  QUERY_INSERT_PEDIDO_PRODUTO =
+    'INSERT INTO pedidos_produtos (numero_pedido, codigo_produto, quantidade, valor_unitario, valor_total) ' + sLineBreak +
+    'VALUES (:numero_pedido, :codigo_produto, :quantidade, :valor_unitario, :valor_total) ';
+
+  QUERY_INSERT_PEDIDO_PRODUTO_LAST_INSERT_ID =
+    'SELECT LAST_INSERT_ID() AS autoincrem ';
+
+  QUERY_CLAUSE_WHERE_BY_AUTOINCREM =
+    ' WHERE autoincrem = :autoincrem ';
+
+  QUERY_PEDIDO_PRODUTO_WHERE_CLAUSE_WHERE_BY_AUTOINCREM = QUERY_PEDIDO_PRODUTOS + sLineBreak + QUERY_CLAUSE_WHERE_BY_AUTOINCREM;
+
+  QUERY_PEDIDO_PRODUTO_BY_NUMERO_PEDIDO = QUERY_PEDIDO_DADOS_GERAIS + sLineBreak + QUERY_CLAUSE_WHERE_BY_AUTOINCREM;
+
+  QUERY_PEDIDO_PRODUTO_CLAUSE_WHERE_BY_NUMERO_PEDIDO = QUERY_PEDIDO_DADOS_GERAIS + sLineBreak + QUERY_CLAUSE_WHERE_BY_AUTOINCREM;
+
+  QUERY_UPDATE_PEDIDO_PRODUTO_BY_AUTOINCREM =
+    'UPDATE pedidos_produtos ' + sLineBreak +
+    '   SET codigo_produto = :codigo_produto, ' + sLineBreak +
+    '       quantidade = :quantidade, ' + sLineBreak +
+    '       valor_unitario = :valor_unitario, ' + sLineBreak +
+    '       valor_total = :quantidade * :valor_unitario ' + sLineBreak +
+    QUERY_CLAUSE_WHERE_BY_AUTOINCREM;
+
+  QUERY_DELETE_PEDIDO_PRODUTO_CLAUSE_WHERE_BY_AUTOINCREM =
+    'DELETE ' + sLineBreak +
+    '  FROM pedidos_produtos ' + sLineBreak +
+    QUERY_CLAUSE_WHERE_BY_AUTOINCREM;
+
+implementation
+
+end.
