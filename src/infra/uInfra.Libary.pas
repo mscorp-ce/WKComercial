a656ca052441bcde0e4d46381e0744eee09731f5
Marcio Silva <m.s.copr@gmail.com>
2025-02-03 14:51:37 -0300

    :sparkles: Modificado a classe "TLibary".
    Arquivo:
    
    - uInfra.Libary.pas
    
    - Removido os métodos, "SwapPointByComma" e "GetStrNumber", tratamentos de números e de "." será feito no OnKeyPress dos TEdit's.

--- src/infra/uInfra.Libary.pas
+++ src/infra/uInfra.Libary.pas
@@ -10,8 +10,6 @@ type
     class function GetINI(Session, Key: String): String;
     class function GetNameAppToINI: String;
     class function Iif(const Condicao: Boolean; Se, SeNao: Variant): Variant;
-    class function SwapPointByComma(const Value: string): string;
-    class function GetStrNumber(const Value: string): string;
   end;
 
 implementation
@@ -52,45 +50,6 @@ begin
   if Condicao then
     Result:= Se
   else Result := SeNao;
-end;
-
-class function TLibary.SwapPointByComma(const Value: string): string;
-var
-  LText : PChar;
-  LResult: String;
-begin
-  LText := PChar(Value);
-  LResult := '';
-
-  while (LText^ <> #0) do
-    begin
-      if LText^ = '.' then
-        LResult := LResult + ','
-      else LResult := LResult+ LText^;
-
-      Inc(LText);
-    end;
-
-  Result:= LResult;
-end;
-
-class function TLibary.GetStrNumber(const Value: string): string;
-var
-  LText : PChar;
-  LResult: String;
-begin
-  LText := PChar(Value);
-  LResult := '';
-
-  while (LText^ <> #0) do
-    begin
-      if LText^ in ['0'..'9'] then
-        LResult := LResult + LText^;
-
-      Inc(LText);
-    end;
-
-  Result := LResult;
 end;
 
 end.
