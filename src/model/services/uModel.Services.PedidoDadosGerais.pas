fc4e25b0c267671b39a65315d02d28c9df4c5e07
Marcio Silva <m.s.copr@gmail.com>
2025-02-04 14:52:10 -0300

    :sparkles: Criado a classe de serviço TPedidoDadosGeraisService.
    Arquivo:
    
    -uModel.Services.PedidoDadosGerais.pas
    
    - Criada a classe TPedidoDadosGeraisService que implementa a interface IDomain<TPedidoDadosGerais>.
    - A classe atua como uma camada de serviço, intermediando operações entre repositórios (PedidoDadosGeraisRepository e ClienteRepository) e a lógica de negócios.
    - Métodos implementados:
      - "Fields": Retorna os campos associados ao pedido de dados gerais via repositório.
      - "CommandSQL": Fornece o comando SQL configurado no repositório.
      - "FindAll": Sobrecargas para buscar todos os registros ou com base em um comando SQL específico.
      - "FindById": Busca um registro pelo ID.
      - "Save", "Update", "DeleteById": Operações de persistência (criação, atualização e exclusão).
      - "IsValid": Validação de entidade com múltiplas sobrecargas, incluindo validação específica para produtos, cliente e data de emissão.
      - "Find": Retorna o ID do registro encontrado.
    - Adicionado construtor e destrutor para gerenciar as instâncias dos repositórios (PedidoDadosGeraisRepository e ClienteRepository) e a dependência IMemory.
    - Utilização de injeção de dependência para receber a interface IMemory no construtor, promovendo flexibilidade e testabilidade.
    - Implementadas validações robustas que lançam exceções específicas ("EServicePedidoDadosGeraisBusinessException") em caso de falhas de negócio.
    - O serviço delega operações de CRUD e consulta aos repositórios, garantindo separação de responsabilidades.

---
+++ src/model/services/uModel.Services.PedidoDadosGerais.pas
@@ -0,0 +1,162 @@
+unit uModel.Services.PedidoDadosGerais;
+
+interface
+
+uses
+  System.Classes, System.Generics.Collections, uModel.Abstraction, uModel.Entities.PedidoDadosGerais,
+  Data.DB, uModel.Entities.Cliente;
+
+type
+  TPedidoDadosGeraisService = class(TInterfacedObject, IDomain<TPedidoDadosGerais>)
+  private
+    FMemory: IMemory;
+    PedidoDadosGeraisRepository: IRepository<TPedidoDadosGerais>;
+    ClienteRepository: IRepository<TCliente>;
+  public
+    function Fields(): TStrings;
+    function FindAll(): TObjectList<TPedidoDadosGerais>; overload;
+    function IsValid(Entity: TPedidoDadosGerais; out MessageContext: String): Boolean; overload;
+    function IsValid(Entity: TPedidoDadosGerais; const Memory: IMemory): Boolean; overload;
+    function Save(Entity: TPedidoDadosGerais): Boolean;
+    function Update(Entity: TPedidoDadosGerais): Boolean; overload;
+    function DeleteById(Entity: TPedidoDadosGerais): Boolean;
+    function Find(): Integer;
+    function FindById(Id: Integer): TPedidoDadosGerais;
+    function CommandSQL(): string;
+    function FindAll(CommandSQL: String): TObjectList<TPedidoDadosGerais>; overload;
+
+    constructor Create(Memory: IMemory); reintroduce;
+    destructor Destroy(); override;
+  end;
+
+implementation
+
+{ TPedidoDadosGeraisService }
+
+uses
+  System.SysUtils, uModel.Repository.PedidoDadosGerais, uModel.uModel.Services.Exception.EServicePedidoDadosGeraisBusinessException,
+  uModel.Repository.Cliente;
+
+function TPedidoDadosGeraisService.CommandSQL(): string;
+begin
+  Result := PedidoDadosGeraisRepository.CommandSQL();
+end;
+
+constructor TPedidoDadosGeraisService.Create(Memory: IMemory);
+begin
+  inherited Create();
+
+  FMemory := Memory;
+
+  PedidoDadosGeraisRepository := TPedidoDadosGeraisRepository.Create();
+  ClienteRepository := TClienteRepository.Create();
+end;
+
+function TPedidoDadosGeraisService.DeleteById(Entity: TPedidoDadosGerais): Boolean;
+begin
+  Result := PedidoDadosGeraisRepository.DeleteById(Entity);
+end;
+
+destructor TPedidoDadosGeraisService.Destroy();
+begin
+  inherited Destroy();
+end;
+
+function TPedidoDadosGeraisService.Fields(): TStrings;
+var
+  Items: TStrings;
+begin
+  Items := PedidoDadosGeraisRepository.Fields;
+
+  Result := Items;
+end;
+
+function TPedidoDadosGeraisService.FindAll(): TObjectList<TPedidoDadosGerais>;
+begin
+  Result:= PedidoDadosGeraisRepository.FindAll();
+end;
+
+function TPedidoDadosGeraisService.FindAll(CommandSQL: String): TObjectList<TPedidoDadosGerais>;
+begin
+  Result := PedidoDadosGeraisRepository.FindAll(CommandSQL);
+end;
+
+function TPedidoDadosGeraisService.Find(): Integer;
+begin
+  Result:= PedidoDadosGeraisRepository.Find();
+end;
+
+function TPedidoDadosGeraisService.FindById(Id: Integer): TPedidoDadosGerais;
+begin
+  Result := PedidoDadosGeraisRepository.FindById(Id);
+end;
+
+function TPedidoDadosGeraisService.IsValid(Entity: TPedidoDadosGerais; const Memory: IMemory): Boolean;
+begin
+  if Memory.Data.IsEmpty() then
+    begin
+      raise EServicePedidoDadosGeraisBusinessException.Create('Informe os produtos.');
+    end;
+
+  if Entity.DataEmissao <= 0 then
+    begin
+      raise EServicePedidoDadosGeraisBusinessException.Create('Informe uma data de emissão válida.');
+    end;
+
+  if Entity.Cliente.Codigo = 0 then
+    begin
+      raise EServicePedidoDadosGeraisBusinessException.Create('Informe um cliente válido.');
+    end;
+
+  var Cliente: TCliente;
+
+  Cliente := ClienteRepository.FindById(Entity.Cliente.Codigo);
+  try
+    if not Assigned(Cliente) then
+      begin
+        raise EServicePedidoDadosGeraisBusinessException.Create('Informe um cliente válido.');
+      end;
+
+    if Cliente.Codigo = 0 then
+      begin
+        raise EServicePedidoDadosGeraisBusinessException.Create('Informe um cliente válido.');
+      end;
+
+  finally
+    if Assigned(Cliente) then
+      FreeAndNil(Cliente);
+  end;
+
+  Result := True;
+end;
+
+function TPedidoDadosGeraisService.IsValid(Entity: TPedidoDadosGerais; out MessageContext: String): Boolean;
+begin
+  Result := False;
+end;
+
+function TPedidoDadosGeraisService.Save(Entity: TPedidoDadosGerais): Boolean;
+begin
+  if not IsValid(Entity, FMemory) then
+    raise EServicePedidoDadosGeraisBusinessException.Create('Erro validar um Pedido Dados Gerais.');
+
+  Result := PedidoDadosGeraisRepository.Save(Entity);
+
+  if not Result then
+    raise EServicePedidoDadosGeraisBusinessException.Create('Erro ao tentar salvar um Pedido Dados Gerais.');
+end;
+
+function TPedidoDadosGeraisService.Update(Entity: TPedidoDadosGerais): Boolean;
+var
+  MessageContext: String;
+begin
+  if not IsValid(Entity, MessageContext) then
+    raise EServicePedidoDadosGeraisBusinessException.Create(MessageContext);
+
+  Result := PedidoDadosGeraisRepository.Update(Entity);
+
+  if not Result then
+    raise EServicePedidoDadosGeraisBusinessException.Create('Erro ao tentar alterar um Pedido Dados Gerais.');
+end;
+
+end.
